3c4e8a00975867a3b6d479252ba148b7596f3c0c
ISPr24-39_BeluakovDS <dan.bel.89@bk.ru>
2022-12-23 11:37:07 +0400

    code fix

--- app/src/main/java/ru/pgk63/focusstart/common/extension/IntentExtensions.kt
+++ app/src/main/java/ru/pgk63/focusstart/common/extension/IntentExtensions.kt
@@ -5,7 +5,8 @@ import android.content.Intent
 import android.net.Uri
 
 fun Context.openBrowser(url: String) = this.startActivity(Intent(Intent.ACTION_VIEW)
-    .setData(Uri.parse(url)))
+    .setData(Uri.parse(if(url.startsWith("https://")
+        || url.startsWith("http://")) url else "https://$url")))
 
 fun Context.openPhone(numberPhone: String) =
     this.startActivity(Intent(Intent.ACTION_CALL, Uri.parse("tel:$numberPhone")))
